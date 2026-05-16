2b263e76eda5e1e667f5876652dc6e911d4cbefd
Piotr Wesu <piotrwesu@gmail.com>
2026-05-16 18:23:10 +0200

    save image with proper extension

--- source/main.c
+++ source/main.c
@@ -1,5 +1,7 @@
 #include "https_client.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "sjson.h"
 
 int main(void)
@@ -16,15 +18,15 @@ int main(void)
     if(json == nullptr) {
         puts("Can't get json file from NASA APOD.");
         error_code = 1;
-        goto cleanup;
+        goto clean_client;
     }
 
-    printf("%s\n", json);
+    //printf("%s\n", json);
 
     SjsonNode *root = Sjson_parse(&json);
     if(root == nullptr){
         puts("Parsing error.\n");
-        goto cleanup;
+        goto clean_client;
     }
 
     SjsonNode *node = Sjson_get_index_array(root, 0);
@@ -37,15 +39,37 @@ int main(void)
     
     SjsonNode *explanation_node = Sjson_get_value(node, "explanation");
     const char *explanation = Sjson_get_string_value(explanation_node);
-    printf("Explanation: %s", explanation);
+    printf("Explanation: %s\n", explanation);
+    
+    char *image_name = strrchr(url_photo, '/') + 1;
+    size_t image_name_size = strlen(image_name);
+    const char temp_path[] = "/tmp/";
+    const int temp_path_size = strlen(temp_path);
 
-    bool ret = https_download_file(client, url_photo, "/tmp/apod.jpg");
-    if(!ret)
+    char *image_file_path = malloc(temp_path_size + image_name_size + 1);
+    if(image_file_path == nullptr) {
+        fprintf(stderr, "Can't allocate memory for image_file_path");
+        error_code = 1;
+        goto cleanup;
+    }
+
+    memcpy(image_file_path , temp_path, temp_path_size);
+    memcpy(image_file_path + temp_path_size, image_name, image_name_size);
+    image_file_path[temp_path_size + image_name_size] = '\0';
+
+    bool download_ret = https_download_file(client, url_photo, image_file_path);
+    if(!download_ret) {
         printf("Can't download image.");
+        error_code = 1;
+        goto cleanup;
+    }
+
+cleanup:
+    free(image_file_path);
 
     Sjson_free_root(root);    
 
-cleanup:
+clean_client:
     https_cleanup(client);
 
     return error_code;
